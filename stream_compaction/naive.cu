813038d982df6f8d14e904eed7007efc3a6debb1
rosekelly6400 <109883728+rosekelly6400@users.noreply.github.com>
2026-09-15 17:09:08 -0400

    shared mem naive arb arr length

--- stream_compaction/naive.cu
+++ stream_compaction/naive.cu
@@ -150,6 +150,57 @@ namespace StreamCompaction {
             odata[idx] = shared_idata[idx];
         }
 
+
+        __global__ void scan_GPU_naive_shared_multiBlock(int n, int log2n, int numBlocks, int blockSize, int* odata, const int* idata)
+        {
+            unsigned idx = blockIdx.x * blockDim.x + threadIdx.x;
+            if (idx >= n) return;
+
+            unsigned sharedIdx = threadIdx.x;
+            extern __shared__ int shared_idata[];
+            shared_idata[sharedIdx] = idata[idx];
+            __syncthreads();
+
+            for (int i = 1; i <= log2n; i++)
+            {
+                int spacing = 1 << (i - 1);
+                if (sharedIdx >= spacing)
+                {
+                    shared_idata[sharedIdx] = shared_idata[sharedIdx - spacing] + shared_idata[sharedIdx];
+                }
+                // CHANGE ABOVE SO IT RUNS HALF THE THREADS ??
+                __syncthreads();
+            }
+            odata[idx] = shared_idata[sharedIdx];
+        }
+
+        __global__ void copyBlockSums(int numBlocks, int blockSize, int* odata, const int* idata)
+        {
+            unsigned idx = blockIdx.x * blockDim.x + threadIdx.x;
+            unsigned originalBufferIdx = ((idx+1)* blockSize) - 1;
+            if (idx >= numBlocks) return;
+
+            odata[idx] = idata[originalBufferIdx];
+        }
+
+        __global__ void addBlockSumsBack(int n, int numBlocks, int blockSize, int* odata, const int* blockSums)
+        {
+            unsigned idx = blockIdx.x * blockDim.x + threadIdx.x;
+            // subtract 1 for inclusive scan since nothing gets added to first block
+            unsigned blockSumIdx = (idx / blockSize)-1;
+            if (idx >= n || blockSumIdx < 0 || blockSumIdx >= numBlocks) return;
+
+            odata[idx] += blockSums[blockSumIdx];
+        }
+
+        __global__ void copyBlockSumsToOutData(int numBlocks, int blockSize, int* odata, const int* idata)
+        {
+            unsigned idx = blockIdx.x * blockDim.x + threadIdx.x;
+            if (idx >= numBlocks) return;
+
+            odata[idx] = idata[idx];
+        }
+
         void scanShared(int n, int* odata, const int* idata) {
             // TODO
             // include zero buffer for non power of 2 n
@@ -159,20 +210,24 @@ namespace StreamCompaction {
             if (n < blockSize) {
                 blockSize = n;
             }
-            else {
-                checkCUDAError("Array length is too big for shared mem approach!");
-            }
 
             int sizeInBytes = dataLength * sizeof(int);
 
+            int numBlocks = (dataLength + blockSize - 1) / blockSize;
+
             int* dev_in;
             int* dev_out;
+            int* dev_blockSums;
+            int* dev_blockSumsScanned;
 
             cudaMalloc((void**)&dev_in, sizeInBytes);
             checkCUDAError("cudaMalloc dev_in failed!");
-
             cudaMalloc((void**)&dev_out, sizeInBytes);
             checkCUDAError("cudaMalloc dev_out failed!");
+            cudaMalloc((void**)&dev_blockSums, numBlocks *sizeof(int));
+            checkCUDAError("cudaMalloc dev_blockSums failed!");
+            cudaMalloc((void**)&dev_blockSumsScanned, numBlocks * sizeof(int));
+            checkCUDAError("cudaMalloc dev_blockSums failed!");
 
             cudaMemcpy(dev_in, idata, sizeInBytes, cudaMemcpyHostToDevice);
 
@@ -194,13 +249,44 @@ namespace StreamCompaction {
             }
 
             // scan using shared mem
-            scan_GPU_naive_shared << <fullBlocksPerGrid, blockSize, sizeInBytes >> > (dataLength, log2n, dev_out, dev_in);
+
+            // 1. scan array as blocks of blockSize
+           /* for (int i = 0; i < numBlocks; i++)
+            {
+                int sharedMemSizeInBytes = blockSize * sizeof(int);
+                int log2BlockSize = ilog2ceil(blockSize);
+                scan_GPU_naive_shared << <fullBlocksPerGrid, blockSize, sharedMemSizeInBytes >> > (blockSize, log2BlockSize, dev_out + (i * blockSize), dev_in + (i * blockSize));
+            }*/
+            int sharedMemSizeInBytes = blockSize * sizeof(int);
+            int log2BlockSize = ilog2ceil(blockSize);
+            scan_GPU_naive_shared_multiBlock << <fullBlocksPerGrid, blockSize, sharedMemSizeInBytes >> > (dataLength, log2BlockSize, numBlocks, blockSize, dev_out, dev_in);
+
+            // 2. Write total sum of each block into a new array
+            dim3 blockSumsBlocksPerGrid((numBlocks + blockSize - 1) / blockSize);
+            copyBlockSums << <blockSumsBlocksPerGrid, blockSize >> > (numBlocks, blockSize, dev_blockSums, dev_out);
+            // 3. Exclusive scan that array (or inclusive scan and ignore last element and add to next section)
+            int blockSumsSharedMemSizeInBytes = numBlocks * sizeof(int);
+            int log2NumBlocks = ilog2ceil(numBlocks);
+            for (int i = 1; i <= log2NumBlocks; i++)
+            {
+                scan_GPU_naive << <fullBlocksPerGrid, blockSize >> > (numBlocks, i, dev_blockSumsScanned, dev_blockSums);
+                if (i != log2NumBlocks)
+                {
+                    int* temp = dev_blockSums;
+                    dev_blockSums = dev_blockSumsScanned;
+                    dev_blockSumsScanned = temp;
+                }
+            }
+            // 4. Add each element back to its section (or next section if inclusive scan)
+            addBlockSumsBack << <fullBlocksPerGrid, blockSize >> > (n, numBlocks, blockSize, dev_out, dev_blockSumsScanned);
 
             // post process to make it an exclusive scan and remove trailing zeros from non power of 2
             postScanFormat << <fullBlocksPerGrid, blockSize >> > (n, dataLength, dev_in, dev_out);
             int* temp = dev_in;
             dev_in = dev_out;
             dev_out = temp;
+
+            setFirstElementToZero << <1, 1 >> > (dataLength, dev_out);
 
             timer().endGpuTimer();
 
